e7ec2e279c6caff9ba0030a5d80d9e12a937993d
Erdem Tuna <30694364+erdemtuna@users.noreply.github.com>
2018-09-03 12:11:15 +0300

    dht11 with server, working

---
+++ dht11-test-code/dht11-test-code.ino
@@ -0,0 +1,59 @@
+//
+//    FILE: dht11_test.ino
+//  AUTHOR: Rob Tillaart
+// VERSION: 0.1.01
+// PURPOSE: DHT library test sketch for DHT11 && Arduino
+//     URL:
+//
+// Released to the public domain
+//
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 2
+float hum, temp_c;
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("DHT TEST PROGRAM ");
+  Serial.print("LIBRARY VERSION: ");
+  Serial.println(DHT_LIB_VERSION);
+  Serial.println();
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+}
+
+void loop()
+{
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+		Serial.print("OK,\t"); 
+		break;
+    case DHTLIB_ERROR_CHECKSUM: 
+		Serial.print("Checksum error,\t"); 
+		break;
+    case DHTLIB_ERROR_TIMEOUT: 
+		Serial.print("Time out error,\t"); 
+		break;
+    default: 
+		Serial.print("Unknown error,\t"); 
+		break;
+  }
+  // DISPLAY DATA
+  hum = round(DHT.humidity*10)/10;
+  temp_c = round(DHT.temperature*10)/10.0;
+  Serial.print(hum);
+  Serial.print(",\t");
+  Serial.println(temp_c);
+
+  delay(2000);
+}
+//
+// END OF FILE
+//
+
